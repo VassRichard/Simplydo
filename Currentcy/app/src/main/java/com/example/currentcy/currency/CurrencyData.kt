a6e9b2f9db20de1f00cb23edb57e3dc2ee2b9fee
Richard <45513482+Maarchosias@users.noreply.github.com>
2021-03-24 18:16:38 +0200

    Delete Currentcy directory

--- Currentcy/app/src/main/java/com/example/currentcy/currency/CurrencyData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.currentcy.currency
-
-class CurrencyData {
-    val id: Int? = null
-    val name: String? = null
-    val userId: Double? = null
-}
--- Currentcy/app/src/main/java/com/example/currentcy/currency/CurrencyViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.currentcy.currency
-
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
-
-class CurrencyViewModel(application: Application) : AndroidViewModel(application) {
-
-}
